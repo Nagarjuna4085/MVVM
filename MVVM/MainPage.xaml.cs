fc80c9e1a24bc90b87c4ab4d9373df7140c4bea0
nagarjnasanam <snagarjuna2001@gmail.com>
2025-04-25 09:10:55 -0400

    Add project files.

---
+++ MVVM/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MVVM
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+       
+        void ConcatAndUpdateName()
+        {
+            LabelFullName.Text = $"{FirstEntry.Text} {LastEntry.Text}";
+        }
+
+        private void FirstEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ConcatAndUpdateName();
+        }
+
+        private void LastEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ConcatAndUpdateName();
+
+        }
+
+   
+
+        private void ClickButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Wishes","GoodMorning","Cancel");
+            //ClickButton.IsEnabled = false;
+            Console.WriteLine("from button clicked evemrr"); 
+            //ClickButton.IsEnabled = true;
+
+        }
+    }
+
+}

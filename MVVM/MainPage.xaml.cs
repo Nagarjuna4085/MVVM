2c226700855a5645f9823884a855648004ace8a6
nagarjnasanam <snagarjuna2001@gmail.com>
2025-04-25 10:49:06 -0400

    implemented with mvvm

--- MVVM/MainPage.xaml.cs
+++ MVVM/MainPage.xaml.cs
@@ -6,34 +6,9 @@
         public MainPage()
         {
             InitializeComponent();
-        }
-       
-        void ConcatAndUpdateName()
-        {
-            LabelFullName.Text = $"{FirstEntry.Text} {LastEntry.Text}";
+            BindingContext = new MainViewModel();
         }
 
-        private void FirstEntry_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            ConcatAndUpdateName();
-        }
-
-        private void LastEntry_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            ConcatAndUpdateName();
-
-        }
-
-   
-
-        private void ClickButton_Clicked(object sender, EventArgs e)
-        {
-            DisplayAlert("Wishes","GoodMorning","Cancel");
-            //ClickButton.IsEnabled = false;
-            Console.WriteLine("from button clicked evemrr"); 
-            //ClickButton.IsEnabled = true;
-
-        }
     }
 
 }
---
+++ MVVM/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MVVM
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        string firstName;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        string lastName;
+
+        public string FullName => $"{firstName} {lastName}";
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        bool isBusy;
+
+        public bool IsNotBusy => !IsBusy;
+
+        [RelayCommand]
+        void Tap()
+        { 
+            IsBusy = false;
+            Console.WriteLine(FullName);
+            IsBusy = true;
+        }
+
+    }
+}
